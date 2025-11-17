53d08b87ba44bfa8e6002381e902559db31636c3
Kosti <kosti.korkiakoski@centria.fi>
2025-11-17 16:13:57 +0200

    Saatu 5/7 bonareista tehty

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -20,25 +20,34 @@ def main():
 
     
     # Määritellään tiedoston nimi suoraan koodissa
+    from datetime import datetime
+    from datetime import timedelta
     varauksetdata = "varaukset.txt"
     varaus_lista = []
     # Avataan tiedosto ja luetaan sisältö
     with open(varauksetdata, "r", encoding="utf-8") as f:
         varaukset = f.readlines()
         for rivi in varaukset:
+
             osat = rivi.strip().split("|")
+            pvm_obj = datetime.strptime(osat[2], "%Y-%m-%d").strftime("%d.%m.%Y") # Muutetaan päivämäärä objektiksi
+            aika = datetime.strptime(osat[3], "%H:%M") # Muutetaan aika objektiksi
+            lopetusaika = aika  + timedelta(hours=float((osat[4]))) # Lasketaan lopetusaika
+            aika_str = aika.strftime("%H.%M")
+            lopetusaika_str = lopetusaika.strftime("%H.%M")
             varaus = {
-                "Varausnumero": osat[0],
-                "Varaaja": osat[1],
-                "Päivämäärä": osat[2],
-                "Aloitusaika": osat[3],
-                "Tuntimäärä": osat[4],
-                "Tuntihinta": osat[5],
-                #"Kokonaishinta": osat[6],
-                "Maksettu": osat[6],
-                "Kohde": osat[7],
-                "Puhelin": osat[8],
-                "Sähköposti": osat[9]
+                "Varausnumero": int(osat[0]),
+                "Varaaja": str(osat[1]),
+                "Päivämäärä": pvm_obj,
+                "Aloitusaika": aika_str,
+                "Lopetusaika": lopetusaika_str,
+                "Tuntimäärä": float(osat[4]),
+                "Tuntihinta": float(osat[5]),
+                "Kokonaishinta": float(osat[4]) * float(osat[5]),
+                "Maksettu": (osat[6].strip().lower() in ["true", "kyllä", "yes", "1"]),
+                "Kohde": str(osat[7]),
+                "Puhelin": str(osat[8]),
+                "Sähköposti": str(osat[9])
             }
             varaus_lista.append(varaus)
     return varaus_lista
@@ -63,4 +72,34 @@ def main():
     """
 
 if __name__ == "__main__":
+
     varaukset = main()
+    for varaus in varaukset:
+       
+        print(f"Varausnumero: {varaus['Varausnumero']}")
+        print(f"Varaaja: {varaus['Varaaja']}")
+        print(f"Päivämäärä: {varaus['Päivämäärä']}") 
+        print(f"Aloitusaika: {varaus['Aloitusaika']}")
+        print(f"Tuntimäärä: {varaus['Tuntimäärä']}"" h")
+        print(f"Lopetusaika: {varaus['Lopetusaika']}")
+        print(f"Tuntihinta: {str(varaus['Tuntihinta']).replace('.',',')} €")
+        print(f"Kokonaishinta: {str(varaus['Kokonaishinta']).replace('.',',')} €")
+        if varaus['Maksettu']:
+            print("Maksettu: Kyllä")
+        else:
+            print("Maksettu: Ei")
+        print(f"Kohde: {varaus['Kohde']}")
+        print(f"Puhelin: {varaus['Puhelin']}")
+        print(f"Sähköposti: {varaus['Sähköposti']}")
+        print()  # Tyhjä rivi varausten väliin
+
+    vastaus = input("Haluatko tietää varausten yhteismäärän? Syötä ""Kyllä"" tai ""Ei"": ").strip().lower()
+    if vastaus in ["kyllä", "yes", "joo"]:
+        kaikkienvaraustensumma = 0
+        for varaus in varaukset:
+            kaikkienvaraustensumma += float(varaus["Kokonaishinta"])
+        print("...") #jätetään vähä väliä    
+        print(f"Kaikkien varausten yhteismäärä on: {str(kaikkienvaraustensumma).replace('.',',')} €")
+        print("Kiitos ohjelman käytöstä!")
+    else:
+        print("Kiitos ohjelman käytöstä!")
